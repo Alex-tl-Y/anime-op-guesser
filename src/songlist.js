191f06be86783d3d2e485409c0286b5e8aed627c
Alex-tl-Y <alextl.younguoft2@gmail.com>
2025-08-01 22:20:39 -0400

    Added more songs. HTML change to try and make it functionally on github pages.

--- src/songlist.js
+++ src/songlist.js
@@ -11,6 +11,166 @@ export const songList = [{
 {
   name: 'Steins Gate',
   songLink: 'https://www.youtube.com/embed/1FPdtR_5KFo?si=aliaNwdASFsdJ0dp'
+},
+
+{
+  name: 'Neon Genesis Evangelion',
+  songLink: 'https://www.youtube.com/watch?v=fShlVhCfHig'
+},
+
+{
+  name: 'Kuroko No Basket',
+  songLink: 'https://www.youtube.com/watch?v=uX53ttaY-oo'
+},
+
+{
+  name: 'Cowboy Bebop',
+  songLink: 'https://www.youtube.com/watch?v=EL-D9LrFJd4'
+},
+
+{
+  name: 'Madoka Magica',
+  songLink: 'https://www.youtube.com/watch?v=OrgpX-_bFqM'
+},
+
+{
+  name: 'Bakemonogatari',
+  songLink: 'https://www.youtube.com/watch?v=CXDUXjsU5ao'
+},
+
+{
+  name: 'Hunter X Hunter',
+  songLink: 'https://www.youtube.com/watch?v=faqmNf_fZlE'
+},
+
+{
+  name: 'K ON',
+  songLink: 'https://www.youtube.com/watch?v=qN41mx4tA0I'
+},
+
+{
+  name: 'Fate/Stay Night',
+  songLink: 'https://www.youtube.com/watch?v=7vZp3yGxZXE'
+},
+
+{
+  name: 'Mirai Nikki',
+  songLink: 'https://www.youtube.com/watch?v=LLDA9cfRLlg'
+},
+
+{
+  name: 'Code Geass',
+  songLink: 'https://www.youtube.com/watch?v=G8CFuZ9MseQ'
+},
+
+{
+  name: 'Tokyo Ghoul',
+  songLink: 'https://www.youtube.com/watch?v=gjD_x4222Q8'
+},
+
+{
+  name: 'Noragami', 
+  songLink: 'https://www.youtube.com/watch?v=gWCnKoEgfP0'
+},
+
+{
+  name: 'No Game No Life',
+  songLink: 'https://www.youtube.com/watch?v=6CBp4qylX6I'
+},
+
+{
+  name: 'Mob Psycho 100',
+  songLink: 'https://www.youtube.com/watch?v=Bw-5Lka7gPE'
+},
+
+{
+  name: 'The Apothecary Diaries',
+  songLink: 'https://www.youtube.com/watch?v=EQ-DKvLQlyQ'
+},
+
+{
+  name: 'Re: Zero',
+  songLink: 'https://www.youtube.com/watch?v=0Vwwr3VGsYg'
+},
+
+{
+  name: 'Konosuba',
+  songLink: 'https://www.youtube.com/watch?v=fpG3BPNQepY'
+},
+
+{
+  name: 'Jujutsu Kaisen',
+  songLink: 'https://www.youtube.com/watch?v=GwaRztMaoY0'
+},
+
+{
+  name: 'My Hero Academia',
+  songLink: 'https://www.youtube.com/watch?v=Q7w5IMyJ3pM'
+},
+
+{
+  name: 'Attack on Titan',
+  songLink: 'https://www.youtube.com/watch?v=8OkpRK2_gVs'
+},
+
+{
+  name: 'Attack on Titan',
+  songLink: 'https://www.youtube.com/watch?v=LKP-vZvjbh8'
+},
+
+{
+  name: 'Naruto Shippuden',
+  songLink: 'https://www.youtube.com/watch?v=2upuBiEiXDk'
+},
+
+{
+  name: 'Naruto Shippuden',
+  songLink: 'https://www.youtube.com/watch?v=zVgKnfN9i34'
+},
+
+{
+  name: 'One Piece',
+  songLink: 'https://www.youtube.com/watch?v=IHhNTt3oxtY'
+},
+
+{
+  name: 'Oshi no Ko',
+  songLink: 'https://www.youtube.com/watch?v=ZYlaUrj2Zkk'
+},
+
+{
+  name: 'Tokyo Revengers',
+  songLink: 'https://www.youtube.com/watch?v=By_JYrhx-WY'
+},
+
+{
+  name: 'Spice and Wolf',
+  songLink: 'https://www.youtube.com/watch?v=8IkKUBJ26fo'
+},
+
+{
+  name: 'Rent a Girlfriend',
+  songLink: 'https://www.youtube.com/watch?v=cM1WSovcn4I'
+},
+
+{
+  name: 'Domestic Girlfriend',
+  songLink: 'https://www.youtube.com/watch?v=SaKdpMnwZf0'
+},
+
+{
+  name: 'Soul Eater', 
+  songLink: 'https://www.youtube.com/watch?v=-eYK3YP524A'
+},
+
+{
+  name: 'Ranking of Kings',
+  songLink: 'https://www.youtube.com/watch?v=dWZAH5w8jkQ'
+},
+
+{
+  name: 'Ranking of Kings',
+  songLink: 'https://www.youtube.com/watch?v=s5SSAXK6bQE'
 }
 ];
 
